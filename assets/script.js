7227f073411bc8af28251667ce7e524abc9975a9
Amin <104219888+Amin-Gz@users.noreply.github.com>
2023-05-13 17:14:17 +0330

    with js counter

--- assets/script.js
+++ assets/script.js
@@ -6,45 +6,40 @@ let timerCircle = document.querySelector('.c100');
 let timerNum = document.querySelector('.c100 > span');
 let loadingMessage = document.querySelector('.message .loading')
 let successMessage = document.querySelector('.message .success')
-
-startCounter.addEventListener('click' , function(e) {
+startCounter.addEventListener('click',function(e){
+    console.log(e)
     let seconds = parseInt(inputCounter.value)
-
-    if(isNaN(seconds)) {
-        errorElement.textContent = 'زمان را به درستی وارد کنید';
-        errorElement.classList.add('active')
+    // console.log(inputCounter.value)
+    // console.log(seconds)
+    
+    if (isNaN(seconds)){
+        errorElement.textContent = "زمان خود را به درستی وارد کنید";
+        errorElement.classList.add("active");
         return;
     }
 
     errorElement.classList.remove('active');
-    startBox.classList.remove('active')
-    timerCircle.style.display = 'block';
-    timerNum.textContent = seconds;
-    loadingMessage.style.display = 'block';
-    successMessage.style.display = 'none';            
-    
+    startBox.classList.remove('active');
+    timerCircle.style.display = 'Block';
+    timerNum.textContent = seconds
+    loadingMessage.style.display = "block"
+    successMessage.style.display = "none"
 
-    let originalSeconds = seconds;
-    let lastPercent = 'p100'
-    let timerId = setInterval(() => {
-        if(lastPercent) timerCircle.classList.remove(lastPercent)
 
-        if(seconds <= 0) {
-            clearInterval(timerId);
+    let timerId = setInterval(()=>{
+        if (seconds <= 1){
+            clearInterval(timerId)
             startBox.classList.add('active')
-            timerCircle.style.display = 'none';
-            loadingMessage.style.display = 'none';
-            successMessage.style.display = 'block';            
+            timerCircle.style.display = 'none'
             inputCounter.value = ''
-            return;
+            loadingMessage.style.display = "none"
+            successMessage.style.display = "block"
+
         }
-
-
         seconds -= 1;
-        let percent = Math.abs(Math.floor((( (originalSeconds - seconds) / originalSeconds) * 100) - 100))
-        lastPercent = `p${percent}`;
-        timerCircle.classList.add(`p${percent}`)
-        timerNum.textContent = seconds;
-    }, 1000);
+        timerNum.textContent = seconds
+
+
+    },1000)
 
 })
